b882fdfd06b58b72e20bb5788b2ae99f1012aeca
Dos4 <76490431+Dos4@users.noreply.github.com>
2023-10-29 16:33:16 +0200

    Add evasion system

--- HeroStats/HeroStats/Program.cs
+++ HeroStats/HeroStats/Program.cs
@@ -49,6 +49,15 @@ class Intelligence : Stat
     }
 }
 
+class Evasion : Stat
+{
+    public Evasion(string name, double value)
+    {
+        Name = name;
+        Value = value;
+    }
+}
+
 class Hero
 {
     public string Name { set; get; }
@@ -59,8 +68,10 @@ class Hero
     public Agility Agility { get; set; }
     public Damage Damage { get; set; }
     public Intelligence Intelligence { get; set; }
+    public Evasion Evasion { get; set; }
 
-    public Hero(Health health, Strength strength, Damage damage, Agility agility, Intelligence intelligence, string name)
+    public Hero(Health health, Strength strength, Damage damage, Agility agility, Intelligence intelligence, 
+                Evasion evasion, string name)
     {
         Name = name;
         Health = health;
@@ -68,6 +79,7 @@ class Hero
         Damage = damage;
         Agility = agility;
         Intelligence = intelligence;
+        Evasion = evasion;
 
         Health.Value = health.Value + strength.Value * 1.6;
         Damage.Value = damage.Value + agility.Value * 1.2;
@@ -76,28 +88,54 @@ class Hero
 
     public void Attack(Hero hero)
     {
+        bool chanceOfMiss = false; 
+
         Health.Value = Health.Value + Strength.Value * 1.6;
         Damage.Value = Damage.Value + Agility.Value * 1.2;
 
         Console.WriteLine($"{Name} with {string.Format("{0:0}", Health.Value)}HP attacked {hero.Name} " +
                           $"with {string.Format("{0:0}", hero.Health.Value)}HP");
 
-        Console.WriteLine($"{hero.Name} -{string.Format("{0:0}", Damage.Value)}hp\n");
-        hero.Health.Value -= Damage.Value;
+        chanceOfMiss = ChanceMiss(hero);
 
-        if (hero.Health.Value <= 0)
+        if (chanceOfMiss == false)
         {
-            Kill(hero);
-        }
-        else if (Health.Value <= 0)
-        {
-            Console.WriteLine($"{Name} is dead");
+            Console.WriteLine($"{hero.Name} -{string.Format("{0:0}", Damage.Value)}hp\n");
+            hero.Health.Value -= Damage.Value;
+
+            if (hero.Health.Value <= 0)
+            {
+                Kill(hero);
+            }
+            else if (Health.Value <= 0)
+            {
+                Console.WriteLine($"{Name} is dead");
+            }
+            else
+            {
+                Console.WriteLine($"{hero.Name} now have {string.Format("{0:0}", hero.Health.Value)}HP\n");
+            }
         }
         else
         {
-            Console.WriteLine($"{hero.Name} now have {string.Format("{0:0}", hero.Health.Value)}HP\n");
+            Console.WriteLine("\nMiss\n");
         }
+    }
 
+    private bool ChanceMiss(Hero hero)
+    {
+        int chance = 0;
+        Random rnd = new Random();
+        chance = rnd.Next(0, 100);
+
+        if (chance > hero.Evasion.Value)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     private void Kill(Hero hero)
@@ -115,8 +153,9 @@ class Hero
 
 class Healer : Hero
 {
-    public Healer(Health health, Strength strength, Damage damage, Agility agility, Intelligence intelligence, string name)
-         : base(health, strength, damage, agility, intelligence, name) { }
+    public Healer(Health health, Strength strength, Damage damage, Agility agility, Intelligence intelligence, 
+                  Evasion evasion, string name)
+         : base(health, strength, damage, agility, intelligence, evasion, name) { }
 
     public void Heal(Hero hero)
     {
@@ -146,13 +185,16 @@ internal class Program
     static void Main(string[] args)
     {
         Hero axe = new Hero(new Health("hp", 100), new Strength("strength", 2),
-                            new Damage("damage", 13), new Agility("agility", 4), new Intelligence("intelligence", 3), "Axe");
+                            new Damage("damage", 13), new Agility("agility", 4), new Intelligence("intelligence", 3),
+                            new Evasion("evasion",40), "Axe");
 
         Hero lina = new Hero(new Health("hp", 30), new Strength("strength", 1),
-                            new Damage("damage", 13), new Agility("agility", 8), new Intelligence("intelligence", 6), "Lina");
+                             new Damage("damage", 13), new Agility("agility", 8), new Intelligence("intelligence", 6),
+                             new Evasion("evasion",65), "Lina");
 
         Healer witchDoctor = new Healer(new Health("hp", 100), new Strength("strength", 0.5),
-                            new Damage("damage", 20), new Agility("agility", 2), new Intelligence("intelligence", 7), "Witch Doctor");
+                             new Damage("damage", 20), new Agility("agility", 2), new Intelligence("intelligence", 7), 
+                             new Evasion("evasion",35), "Witch Doctor");
 
         axe.Attack(lina);
 
